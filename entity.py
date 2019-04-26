1af4f394d112c40805e1c6f180df7a28feecfa9c
macaw2000 <macaw2000@hotmail.com>
2019-04-25 17:12:48 -0700

    next one

--- entity.py
+++ entity.py
@@ -2,7 +2,6 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
-
     def __init__(self, x, y, char, color):
         self.x = x
         self.y = y
@@ -10,4 +9,6 @@ class Entity:
         self.color = color
 
     def move(self, dx, dy):
-        
+        # Move the entity by a given amount
+        self.x += dx
+        self.y += dy
