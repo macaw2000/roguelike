7613a8ac6e6307a6fdacab46b9c5a1b563c656b8
macaw2000 <macaw2000@hotmail.com>
2021-09-12 13:02:25 -0700

    chap 7

--- engine.py
+++ engine.py
@@ -4,9 +4,12 @@ from typing import TYPE_CHECKING
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.libtcodpy import console_rect
 from tcod.map import compute_fov
 
 from input_handlers import MainGameEventHandler
+from message_log import MessageLog
+from render_functions import render_bar
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -19,6 +22,7 @@ class Engine:
 
     def __init__(self, player: Actor):
         self.event_handler: EventHandler = MainGameEventHandler(self)
+        self.message_log = MessageLog()
         self.player = player
 
     def handle_enemy_turns(self) -> None:
@@ -39,10 +43,13 @@ class Engine:
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
-        console.print(
-            x=1,
-            y=47,
-            string=f"HP: {self.player.fighter.hp}/{self.player.fighter.max_hp}",
+        self.message_log.render(console=console, x=21, y=45, width=40, height=5)
+
+        render_bar(
+            console=console,
+            current_value=self.player.fighter.hp,
+            maximum_value=self.player.fighter.max_hp,
+            total_width=20,
         )
 
         context.present(console)
