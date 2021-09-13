f78155af74b4a8b0ff8637810c64ed677ab65f0e
macaw2000 <macaw2000@hotmail.com>
2021-09-12 17:44:21 -0700

    chap 8

--- components/base_component.py
+++ components/base_component.py
@@ -2,14 +2,20 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from tcod.libtcodpy import parser_new_struct
+
 if TYPE_CHECKING:
     from engine import Engine
     from entity import Entity
-
+    from game_map import GameMap
 
 class BaseComponent:
-    entity: Entity  # Owning entity instance.
+    parent: Entity  # Owning entity instance.
+
+    @property
+    def gamemap(self) -> GameMap:
+        return self.parent.gamemap
 
     @property
     def engine(self) -> Engine:
-        return self.entity.gamemap.engine
+        return self.gamemap.engine
